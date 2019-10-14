1d6e2cf825639100661ad49e8a28395eb938fd13
Vadim Valeev <vvaleev95@mail.ru>
2019-10-14 09:13:18 +0500

    fixes

--- scripts/js/scripts/ui.js
+++ scripts/js/scripts/ui.js
@@ -1,24 +1,23 @@
 (function(window, document) {
     'use strict';
 
-    var element    = window.Element;
     var semverData = {};
     var domSemverTitle;
-    var domSemverProjectCaption;
-    var domSemverDeleteProject;
+    var domSemverTitleProject;
+    var domSemverButtonDeleteProject;
     var domSemverStartScreen;
     var domSemverSecondScreen;
     var domSemverThirdScreen;
-    var domSemverStartButton;
+    var domSemverButtonToCreateProject;
     var domSemverUpdateButton;
     var domSemverAddButton;
     var domSemverColumn1;
     var domSemverColumn2;
     var domSemverTableBody;
     var domSemverTableBodyTemplate;
-    var domSemverFormVersionsBody;
-    var domSemverFormVersionsBodyTemplate;
-    var domSemverModalAddProjectBodyTemplate;
+    var domSemverFormListBodyVersions;
+    var domSemverFormListBodyVersionsTemplate;
+    var domSemverAddProjectModalWindowBodyTemplate;
 
     window.addEventListener('load', function(ev) {
         //semverData['PROJECT_NAME'] = 'PROJECT_NAME';
@@ -37,79 +36,23 @@
         render();
     });
 
-    element.prototype.setInnerText = function(text) {
-        if(text) {
-            if(this && this instanceof HTMLElement) {
-                if(typeof this.innerText !== 'undefined') {
-                    this.innerText = text;
-                }
-            }
-        }
-    };
-
-    element.prototype.setInnerHTML = function(HTML) {
-        if(HTML) {
-            if(this && this instanceof HTMLElement) {
-                if(typeof this.innerHTML !== 'undefined') {
-                    this.innerHTML = HTML;
-                }
-            }
-        }
-    };
-
-    element.prototype.getInnerText = function() {
-        if(this && this instanceof HTMLElement) {
-            if(typeof this.innerText !== 'undefined' && this.innerText) {
-                return this.innerText;
-            }
-        }
-    };
-
-    element.prototype.getInnerHTML = function() {
-        if(this && this instanceof HTMLElement) {
-            if(typeof this.innerHTML !== 'undefined' && this.innerHTML) {
-                return this.innerHTML;
-            }
-        }
-    };
-
-    element.prototype.hide = function() {
-        if(this && this instanceof HTMLElement) {
-            if(typeof this.style !== 'undefined') {
-                if(this.style.display !== 'none') {
-                    this.style.display = 'none';
-                }
-            }
-        }
-    };
-
-    element.prototype.show = function() {
-        if(this && this instanceof HTMLElement) {
-            if(typeof this.style !== 'undefined') {
-                if(this.style.display === 'none') {
-                    this.style.display = null;
-                }
-            }
-        }
-    };
-
     function initDomElements() {
-        domSemverTitle                       = document.querySelector('[data-semver="title"]');
-        domSemverProjectCaption              = document.querySelector('[data-semver="project-caption"]');
-        domSemverDeleteProject               = document.querySelector('[data-semver="delete-project"]');
-        domSemverStartScreen                 = document.querySelector('[data-semver="start-screen"]');
-        domSemverSecondScreen                = document.querySelector('[data-semver="second-screen"]');
-        domSemverThirdScreen                 = document.querySelector('[data-semver="third-screen"]');
-        domSemverStartButton                 = document.querySelector('[data-semver="start-button"]');
-        domSemverUpdateButton                = document.querySelector('[data-semver="update-button"]');
-        domSemverAddButton                   = document.querySelector('[data-semver="add-button"]');
-        domSemverColumn1                     = document.querySelector('[data-semver="column-1"]');
-        domSemverColumn2                     = document.querySelector('[data-semver="column-2"]');
-        domSemverTableBody                   = document.querySelector('[data-semver="table-body"]');
-        domSemverTableBodyTemplate           = document.querySelector('[data-semver="table-body-template"]');
-        domSemverFormVersionsBody            = document.querySelector('[data-semver="form-versions-body"]');
-        domSemverFormVersionsBodyTemplate    = document.querySelector('[data-semver="form-versions-body-template"]');
-        domSemverModalAddProjectBodyTemplate = document.querySelector('[data-semver="modal-add-project-body-template"]');
+        domSemverTitle                             = document.querySelector('[data-semver="title"]');
+        domSemverTitleProject                      = document.querySelector('[data-semver="title-project"]');
+        domSemverButtonDeleteProject               = document.querySelector('[data-semver="button-delete-project"]');
+        domSemverStartScreen                       = document.querySelector('[data-semver="start-screen"]');
+        domSemverSecondScreen                      = document.querySelector('[data-semver="second-screen"]');
+        domSemverThirdScreen                       = document.querySelector('[data-semver="third-screen"]');
+        domSemverButtonToCreateProject             = document.querySelector('[data-semver="button-to-create-project"]');
+        domSemverUpdateButton                      = document.querySelector('[data-semver="update-button"]');
+        domSemverAddButton                         = document.querySelector('[data-semver="add-button"]');
+        domSemverColumn1                           = document.querySelector('[data-semver="column-1"]');
+        domSemverColumn2                           = document.querySelector('[data-semver="column-2"]');
+        domSemverTableBody                         = document.querySelector('[data-semver="table-body"]');
+        domSemverTableBodyTemplate                 = document.querySelector('[data-semver="table-body-template"]');
+        domSemverFormListBodyVersions              = document.querySelector('[data-semver="form-list-body-versions"]');
+        domSemverFormListBodyVersionsTemplate      = document.querySelector('[data-semver="form-list-body-versions-template"]');
+        domSemverAddProjectModalWindowBodyTemplate = document.querySelector('[data-semver="add-project-modal-window-body-template"]');
     }
 
     function initHandlers() {
@@ -143,14 +86,14 @@
             }
         });
 
-        domSemverStartButton && (domSemverStartButton.onclick = function(ev) {
+        domSemverButtonToCreateProject && (domSemverButtonToCreateProject.onclick = function(ev) {
             ev.preventDefault();
 
             Modal.options.focus = '[name="PROJECT_NAME"]';
-            Modal.open(domSemverModalAddProjectBodyTemplate, 'inline');
+            Modal.open(domSemverAddProjectModalWindowBodyTemplate, 'inline');
         });
 
-        domSemverDeleteProject && (domSemverDeleteProject.onclick = function(ev) {
+        domSemverButtonDeleteProject && (domSemverButtonDeleteProject.onclick = function(ev) {
             ev.preventDefault();
 
             semverData = {};
@@ -161,21 +104,21 @@
 
     function render() {
         domSemverTitle && domSemverTitle.setInnerText(messages['CAPTIONS']['TITLE']);
-        domSemverStartButton && domSemverStartButton.setInnerText(messages['BUTTONS']['ADD_PROJECT']);
+        domSemverButtonToCreateProject && domSemverButtonToCreateProject.setInnerText(messages['BUTTONS']['ADD_PROJECT']);
         domSemverUpdateButton && domSemverUpdateButton.setInnerText(messages['BUTTONS']['UPDATE']);
         domSemverAddButton && domSemverAddButton.setInnerText(messages['BUTTONS']['ADD']);
         domSemverColumn1 && domSemverColumn1.setInnerText(messages['CAPTIONS']['DATE']);
         domSemverColumn2 && domSemverColumn2.setInnerText(messages['CAPTIONS']['VERSION']);
 
-        domSemverModalAddProjectBodyTemplate && domSemverModalAddProjectBodyTemplate.setInnerHTML(renderTemplate(['{{PROJECT_NAME}}'], [messages['CAPTIONS']['PROJECT']], domSemverModalAddProjectBodyTemplate.getInnerHTML()));
+        domSemverAddProjectModalWindowBodyTemplate && domSemverAddProjectModalWindowBodyTemplate.setInnerHTML(renderTemplate(['{{PROJECT_NAME}}'], [messages['CAPTIONS']['PROJECT']], domSemverAddProjectModalWindowBodyTemplate.getInnerHTML()));
 
         if(semverData['PROJECT_NAME']) {
-            domSemverProjectCaption && domSemverProjectCaption.setInnerText(semverData['PROJECT_NAME']);
+            domSemverTitleProject && domSemverTitleProject.setInnerText(semverData['PROJECT_NAME']);
 
             domSemverStartScreen && domSemverStartScreen.hide();
             domSemverSecondScreen && domSemverSecondScreen.show();
             domSemverThirdScreen && domSemverThirdScreen.show();
-            domSemverDeleteProject && domSemverDeleteProject.show();
+            domSemverButtonDeleteProject && domSemverButtonDeleteProject.show();
 
             if(semverData['VERSIONS'] && semverData['VERSIONS'] instanceof Array && semverData['VERSIONS'].length) {
                 renderTemplateTableBody(semverData['VERSIONS'].reverse());
@@ -209,12 +152,12 @@
             }
         }
         else {
-            domSemverProjectCaption && domSemverProjectCaption.setInnerText('SemVerEditor');
+            domSemverTitleProject && domSemverTitleProject.setInnerText('SemVerEditor');
 
             domSemverStartScreen && domSemverStartScreen.show();
             domSemverSecondScreen && domSemverSecondScreen.hide();
             domSemverThirdScreen && domSemverThirdScreen.hide();
-            domSemverDeleteProject && domSemverDeleteProject.hide();
+            domSemverButtonDeleteProject && domSemverButtonDeleteProject.hide();
 
             renderTemplateTableBody([{
                 version : '0.1.0'
@@ -267,7 +210,7 @@
     }
 
     function renderTemplateFormVersionsBody(data) {
-        var template     = domSemverFormVersionsBodyTemplate && domSemverFormVersionsBodyTemplate.getInnerHTML();
+        var template     = domSemverFormListBodyVersionsTemplate && domSemverFormListBodyVersionsTemplate.getInnerHTML();
         var templates;
         var values;
         var templateData = '';
@@ -288,7 +231,7 @@
             }
 
             if(templateData) {
-                domSemverFormVersionsBody && domSemverFormVersionsBody.setInnerHTML(templateData);
+                domSemverFormListBodyVersions && domSemverFormListBodyVersions.setInnerHTML(templateData);
             }
         }
     }
